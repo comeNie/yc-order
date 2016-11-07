e96b9a89ea565964386d5356733067e35a913f03
zhanglh <zhanglh@asiainfo.com>
2016-11-07 15:54:42 +0800

    add 刷新数据文件

---
+++ order-core/src/main/java/com/ai/yc/order/service/business/impl/search/OrderIndexBusiSVImpl.java
@@ -0,0 +1,111 @@
+package com.ai.yc.order.service.business.impl.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.sdk.util.BeanUtils;
+import com.ai.opt.sdk.util.CollectionUtil;
+import com.ai.yc.order.dao.mapper.bo.OrdBalacneIf;
+import com.ai.yc.order.dao.mapper.bo.OrdOdFeeTotal;
+import com.ai.yc.order.dao.mapper.bo.OrdOdProd;
+import com.ai.yc.order.dao.mapper.bo.OrdOdProdExtend;
+import com.ai.yc.order.dao.mapper.bo.OrdOrder;
+import com.ai.yc.order.search.bo.OrdProdExtend;
+import com.ai.yc.order.search.bo.OrderInfo;
+import com.ai.yc.order.service.atom.interfaces.IOrdBalacneIfAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOdFeeTotalAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOdProdAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOdProdExtendAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOrderAtomSV;
+import com.ai.yc.order.service.business.interfaces.search.IOrderIndexBusiSV;
+
+@Service
+public class OrderIndexBusiSVImpl implements IOrderIndexBusiSV {
+	@Autowired
+	private IOrdOrderAtomSV ordOrderAtomSV;
+
+	@Autowired
+	private IOrdOdProdAtomSV ordOdProdAtomSV;
+	@Autowired
+	private IOrdOdProdExtendAtomSV ordOdProdExtendAtomSV;
+	@Autowired
+	private IOrdOdFeeTotalAtomSV ordOdFeeTotalAtomSV;
+	@Autowired
+	private IOrdBalacneIfAtomSV ordBalacneIfAtomSV;
+
+	@Override
+	public List<OrderInfo> orderFillQuery(List<OrdOrder> ordList) throws BusinessException, SystemException {
+
+		List<OrderInfo> ordInfoList = new ArrayList<>();
+		List<OrdProdExtend> ordextendList = new ArrayList<>();
+		for (OrdOrder ord : ordList) {
+			OrderInfo ordInfo = new OrderInfo();
+			ordInfo.setOrderid(ord.getOrderId());
+			ordInfo.setBusitype(ord.getBusiType());
+			ordInfo.setCorpoarid(ord.getCorporaId());
+			ordInfo.setFlag(ord.getFlag());
+			ordInfo.setInterperid(ord.getInterperId());
+			ordInfo.setInterpertype(ord.getInterperType());
+			ordInfo.setTimezone(ord.getTimeZone());
+			ordInfo.setDisplayflag(ord.getDisplayFlag());
+			ordInfo.setFinishtime(ord.getFinishTime());
+			ordInfo.setChlid(ord.getChlId());
+			ordInfo.setLspid(ord.getLspId());
+			ordInfo.setKeywords(ord.getKeywords());
+			ordInfo.setOrderlevel(ord.getOrderLevel());
+			ordInfo.setOrdertype(ord.getOrderType());
+			ordInfo.setStatechgtime(ord.getStateChgTime());
+			ordInfo.setState(ord.getState());
+			ordInfo.setUsertype(ord.getUserType());
+			ordInfo.setUpdateflag(ord.getUpdateFlag());
+			ordInfo.setOrdertime(ord.getOrderTime());
+			ordInfo.setLocktime(ord.getLockTime());
+			//赋值假数据
+			ordInfo.setUsername("test");
+			ordInfo.setLspname("test");
+			//BeanUtils.copyProperties(ordInfo, ord);
+			// 查询商品信息
+			OrdOdProd ordOdProd = ordOdProdAtomSV.findByOrderId(ord.getOrderId());
+			if (ordOdProd != null) {
+				ordInfo.setStarttime(ordOdProd.getStateTime());
+				ordInfo.setEndtime(ordOdProd.getEndTime());
+				ordInfo.setUsecode(ordOdProd.getUseCode());
+				ordInfo.setField(ordOdProd.getFieldCode());
+				ordInfo.setProddetailid(ordOdProd.getProdDetalId());
+			}
+			// 查询语言对信息
+			List<OrdOdProdExtend> ordOdProdExtendList = ordOdProdExtendAtomSV.findByOrderId(ord.getOrderId());
+			if (!CollectionUtil.isEmpty(ordOdProdExtendList)) {
+				for (OrdOdProdExtend extend : ordOdProdExtendList) {
+					OrdProdExtend prodextend = new OrdProdExtend();
+					prodextend.setLangungechname(extend.getLangungePairName());
+					prodextend.setLangungeenname(extend.getLangungeNameEn());
+					prodextend.setLangungeid(extend.getLangungePair());
+					ordextendList.add(prodextend);
+				}
+				ordInfo.setOrdextendes(ordextendList);
+			}
+			// 查询费用信息
+			OrdOdFeeTotal ordOdFeeTotal = ordOdFeeTotalAtomSV.findByOrderId(ord.getOrderId());
+			if (ordOdFeeTotal != null) {
+				ordInfo.setUpdateoperid(ordOdFeeTotal.getUpdateOperId());
+				ordInfo.setUpdatetime(ordOdFeeTotal.getUpdateTime());
+			}
+			// 查询支付信息
+			OrdBalacneIf ordBalacneIf = ordBalacneIfAtomSV.findByOrderId(ord.getOrderId());
+			if (ordBalacneIf != null) {
+				ordInfo.setPaystyle(ordBalacneIf.getPayStyle());
+				ordInfo.setPaytime(ordBalacneIf.getPayTime());
+			}
+			ordInfoList.add(ordInfo);
+			
+		}
+		return ordInfoList;
+	}
+
+}
---
+++ order-core/src/main/java/com/ai/yc/order/service/business/interfaces/search/IOrderIndexBusiSV.java
@@ -0,0 +1,13 @@
+package com.ai.yc.order.service.business.interfaces.search;
+
+import java.util.List;
+
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.base.exception.SystemException;
+import com.ai.yc.order.dao.mapper.bo.OrdOrder;
+import com.ai.yc.order.search.bo.OrderInfo;
+
+public interface IOrderIndexBusiSV {
+	 public List<OrderInfo> orderFillQuery(List<OrdOrder> ordList) throws BusinessException, SystemException;
+
+}
