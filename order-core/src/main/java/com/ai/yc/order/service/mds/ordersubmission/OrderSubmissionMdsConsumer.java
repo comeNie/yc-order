4e141173a0b4e2ddc6e1c8b53fb0711ad6923a49
zhangzhongde <xtgjzzd@163.com>
2016-11-09 16:40:48 +0800

    mds

--- order-core/src/main/java/com/ai/yc/order/service/mds/ordersubmission/OrderSubmissionMdsConsumer.java
+++ order-core/src/main/java/com/ai/yc/order/service/mds/ordersubmission/OrderSubmissionMdsConsumer.java
@@ -2,9 +2,12 @@ package com.ai.yc.order.service.mds.ordersubmission;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.components.mds.base.AbstractMdsConsumer;
@@ -13,12 +16,16 @@ import com.ai.paas.ipaas.mds.IMessageProcessor;
 import com.ai.paas.ipaas.mds.IMsgProcessorHandler;
 import com.ai.yc.order.constants.OrdersConstants;
 import com.ai.yc.order.service.business.interfaces.IOrderSubmissionBusiSV;
-
+@Component
 public class OrderSubmissionMdsConsumer extends AbstractMdsConsumer{
 
 	private static Logger logger = LoggerFactory.getLogger(OrderSubmissionMdsConsumer.class);
 	@Autowired
 	private IOrderSubmissionBusiSV orderSubmissionBusiSV;
+	@PostConstruct
+	public void init() throws Exception{
+		this.startMdsConsumer();
+	}
 	//
 	@Override
 	public void startMdsConsumer() throws Exception {
