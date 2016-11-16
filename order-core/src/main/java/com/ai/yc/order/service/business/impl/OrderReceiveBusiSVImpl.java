65da357010ca7e09b7581559cb246347c7c8a731
zhangzhongde <xtgjzzd@163.com>
2016-11-16 18:06:56 +0800

    领取订单轨迹记录

--- order-core/src/main/java/com/ai/yc/order/service/business/impl/OrderReceiveBusiSVImpl.java
+++ order-core/src/main/java/com/ai/yc/order/service/business/impl/OrderReceiveBusiSVImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.opt.sdk.util.BeanUtils;
 import com.ai.yc.order.api.orderreceive.param.OrderReceiveRequest;
 import com.ai.yc.order.api.orderreceive.param.OrderReceiveResponse;
@@ -11,6 +13,7 @@ import com.ai.yc.order.dao.mapper.bo.OrdOdFeeTotal;
 import com.ai.yc.order.dao.mapper.bo.OrdOrder;
 import com.ai.yc.order.service.atom.interfaces.IOrdOdFeeTotalAtomSV;
 import com.ai.yc.order.service.atom.interfaces.IOrdOrderAtomSV;
+import com.ai.yc.order.service.business.interfaces.IOrdOdStateChgBusiSV;
 import com.ai.yc.order.service.business.interfaces.IOrderReceiveBusiSV;
 
 @Service
@@ -20,6 +23,8 @@ public class OrderReceiveBusiSVImpl implements IOrderReceiveBusiSV {
 	private IOrdOrderAtomSV ordOrderAtomSV;
 	@Autowired
 	private IOrdOdFeeTotalAtomSV ordOdFeeTotalAtomSV;
+	@Autowired
+	private IOrdOdStateChgBusiSV ordOdStateChgBusiSV;
 	
 	@Override
 	@Transactional
@@ -27,14 +32,21 @@ public class OrderReceiveBusiSVImpl implements IOrderReceiveBusiSV {
 		// TODO Auto-generated method stub
 		OrderReceiveResponse response = new OrderReceiveResponse();
 		//
+		OrdOrder ordOrderDb = this.ordOrderAtomSV.findByPrimaryKey(request.getBaseInfo().getOrderId());
+		if (null == ordOrderDb) {
+			throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL, "此订单信息不存在");
+		}
+		
 		OrdOrder ordOrder = new OrdOrder();
 		BeanUtils.copyVO(ordOrder, request.getBaseInfo());
 		//
 		this.ordOrderAtomSV.updateByPrimaryKeySelective(ordOrder);
 		//
 		OrdOdFeeTotal ordOdFeeTotal = new OrdOdFeeTotal();
-		BeanUtils.copyVO(ordOdFeeTotal, request.getFeeInfo());
-		this.ordOdFeeTotalAtomSV.updateByOrderIdSelective(request.getBaseInfo().getOrderId(),ordOdFeeTotal );
+		//BeanUtils.copyVO(ordOdFeeTotal, request.getFeeInfo());
+		//this.ordOdFeeTotalAtomSV.updateByOrderIdSelective(request.getBaseInfo().getOrderId(),ordOdFeeTotal );
+		//
+		this.ordOdStateChgBusiSV.orderReceiveChgDesc(request, request.getBaseInfo().getInterperId(), request.getBaseInfo().getInterperType(), request.getBaseInfo().getLspId(),ordOrderDb.getState());
 		//
 		response.setOrderId(request.getBaseInfo().getOrderId());
 		return response;
