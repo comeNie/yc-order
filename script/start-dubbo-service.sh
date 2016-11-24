0cd0642806583b13c724d52acfa7c92e77e9d2d9
zhangzhongde <xtgjzzd@163.com>
2016-11-24 14:53:31 +0800

    update dockerfile

--- script/start-dubbo-service.sh
+++ script/start-dubbo-service.sh
@@ -36,16 +36,16 @@ sed -i "s%paas.sdk.mode=.*%paas.sdk.mode=${SDK_MODE}%g" ${APP_HOME}/config/paas/
 sed -i "s%ccs.appname=.*%ccs.appname=${CCS_NAME}%g" ${APP_HOME}/config/paas/paas-conf.properties
 sed -i "s%ccs.zk_address=.*%ccs.zk_address=${ZK_ADDR}%g" ${APP_HOME}/config/paas/paas-conf.properties
 #用户信息
-sed -i "s%user.appkey=.*%user.appkey=${USER_APPKEY}%g" ${APP_HOME}/config/remote_url.properties
-sed -i "s%user.query.url=.*%user.query.url=${USER_QUERY_URL}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%user.appkey=.*%user.appkey=${USER_APPKEY}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%user.query.url=.*%user.query.url=${USER_QUERY_URL}%g" ${APP_HOME}/config/remote_url.properties
 #物流信息
-sed -i "s%ofc.appkey=.*%ofc.appkey=${OFC_APPKEY}%g" ${APP_HOME}/config/remote_url.properties
-sed -i "s%ofc.query.url=.*%ofc.query.url=${OFC_QUERY_URL}%g" ${APP_HOME}/config/remote_url.properties
-sed -i "s%ofc.order.create.url=.*%ofc.order.create.url=${OFC_ORDER_CREATE_URL}%g" ${APP_HOME}/config/remote_url.properties
-sed -i "s%ofc.return.create.url=.*%ofc.return.create.url=${OFC_RETURN_CREATE_URL}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%ofc.appkey=.*%ofc.appkey=${OFC_APPKEY}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%ofc.query.url=.*%ofc.query.url=${OFC_QUERY_URL}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%ofc.order.create.url=.*%ofc.order.create.url=${OFC_ORDER_CREATE_URL}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%ofc.return.create.url=.*%ofc.return.create.url=${OFC_RETURN_CREATE_URL}%g" ${APP_HOME}/config/remote_url.properties
 #积分比率
-sed -i "s%ofc.sign=.*%ofc.sign=${OFC_SIGN}%g" ${APP_HOME}/config/remote_url.properties
-sed -i "s%integral.rate.url=.*%integral.rate.url=${INTEGRAL_RATE_URL}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%ofc.sign=.*%ofc.sign=${OFC_SIGN}%g" ${APP_HOME}/config/remote_url.properties
+#sed -i "s%integral.rate.url=.*%integral.rate.url=${INTEGRAL_RATE_URL}%g" ${APP_HOME}/config/remote_url.properties
 
 
 
