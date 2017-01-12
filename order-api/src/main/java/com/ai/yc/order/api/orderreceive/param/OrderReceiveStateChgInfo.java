286c69ab92ca42599cf81052f660ebd01d93c62c
zhangzhongde <xtgjzzd@163.com>
2017-01-12 14:43:46 +0800

    update

--- order-api/src/main/java/com/ai/yc/order/api/orderreceive/param/OrderReceiveStateChgInfo.java
+++ order-api/src/main/java/com/ai/yc/order/api/orderreceive/param/OrderReceiveStateChgInfo.java
@@ -9,7 +9,7 @@ public class OrderReceiveStateChgInfo implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	/**
-	 * 操作者名称，下单的时候前台登录的用户名
+	 * 操作者名称，前台登录的用户名
 	 */
 	private String operName;
 
