290df5ff2c47f51d5d8af4f3f23f91239cb63eee
fazhicao <409993207@qq.com>
2017-02-21 17:59:31 +0800

    订单评价业务写入

--- order-core/src/main/java/com/ai/yc/order/service/business/impl/OrdEvaluateBusiSVImpl.java
+++ order-core/src/main/java/com/ai/yc/order/service/business/impl/OrdEvaluateBusiSVImpl.java
@@ -3,19 +3,40 @@ package com.ai.yc.order.service.business.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.constants.ExceptCodeConstants;
+import com.ai.opt.sdk.util.BeanUtils;
+import com.ai.opt.sdk.util.DateUtil;
+import com.ai.yc.order.api.orderevaluation.param.OrderEvaluationExtendInfo;
+import com.ai.yc.order.api.orderevaluation.param.OrderEvaluationRequest;
+import com.ai.yc.order.api.orderevaluation.param.OrderEvaluationResponse;
 import com.ai.yc.order.api.orderevaluation.param.QueryOrdEvaluteRequest;
 import com.ai.yc.order.api.orderevaluation.param.QueryOrdEvaluteResponse;
+import com.ai.yc.order.constants.OrdOdStateChgConstants;
 import com.ai.yc.order.constants.OrdersConstants;
 import com.ai.yc.order.constants.ResultCodeConstants;
 import com.ai.yc.order.dao.mapper.bo.OrdEvaluate;
+import com.ai.yc.order.dao.mapper.bo.OrdOdStateChg;
+import com.ai.yc.order.dao.mapper.bo.OrdOrder;
+import com.ai.yc.order.orderevaluation.rule.OrderEvaluationRuleSV;
 import com.ai.yc.order.service.atom.interfaces.IOrdEvaluateAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOdStateChgAtomSV;
+import com.ai.yc.order.service.atom.interfaces.IOrdOrderAtomSV;
 import com.ai.yc.order.service.business.interfaces.IOrdEvaluateBusiSV;
+import com.ai.yc.order.util.SequenceUtil;
 
 @Service
 public class OrdEvaluateBusiSVImpl implements IOrdEvaluateBusiSV {
 	@Autowired
 	private IOrdEvaluateAtomSV ordEvaluateAtomSV;
+	@Autowired
+	private OrderEvaluationRuleSV orderEvaluationRuleSV; //订单规则
+	@Autowired
+	private IOrdOrderAtomSV ordOrderAtomSV;//订单主表
+	@Autowired
+	private IOrdOdStateChgAtomSV ordOdStateChgAtomSV;//订单轨迹表
+	
 	@Override
 	public QueryOrdEvaluteResponse queryEvaluteByOrdId(QueryOrdEvaluteRequest request) {
 		QueryOrdEvaluteResponse respponse = new QueryOrdEvaluteResponse();
@@ -33,6 +54,64 @@ public class OrdEvaluateBusiSVImpl implements IOrdEvaluateBusiSV {
 		}
 		respponse.setResponseHeader(new ResponseHeader(true, ResultCodeConstants.SUCCESS_CODE, "查询成功"));
 		return respponse;
+	}
+	
+	
+	@Override
+	public OrderEvaluationResponse orderEvaluation(OrderEvaluationRequest request) {
+		OrdEvaluate evaluate = ordEvaluateAtomSV.findByOrderId(request.getBaseInfo().getOrderId());
+		if(evaluate!=null) {
+			throw new BusinessException(ExceptCodeConstants.Special.SYSTEM_ERROR, "评价信息已存在,无法再次评价"); 
+		}
+		OrderEvaluationResponse response=new OrderEvaluationResponse();
+		
+		OrderEvaluationExtendInfo extendInfo = request.getExtendInfo();
+		Long serveQuality = extendInfo.getServeQuality();
+		Long serveSpeen = extendInfo.getServeSpeen();
+		Long serveManner = extendInfo.getServeManner();
+		// 1.根据规则计算评论等级
+		Integer evaluationRule = orderEvaluationRuleSV.orderEvaluationRule(serveQuality.intValue(), 
+				serveSpeen.intValue(), serveManner.intValue());
+		//2.评价信息入库
+		OrdEvaluate ordEvaluate=new OrdEvaluate();
+		BeanUtils.copyVO(ordEvaluate, extendInfo);
+		ordEvaluate.setEvaluateId(SequenceUtil.createEvaluateId()); 
+		ordEvaluate.setOrderId(request.getBaseInfo().getOrderId());
+		ordEvaluate.setEvaluateSun(String.valueOf(evaluationRule)); //1：差评；2：中评；3：好评；
+		ordEvaluateAtomSV.insertSelective(ordEvaluate);
+		
+		//3.修改订单主表状态字段
+		//3.1 先查询订单主表信息
+		OrdOrder ordOrderDb = this.ordOrderAtomSV.findByPrimaryKey(request.getBaseInfo().getOrderId());
+		if(ordOrderDb==null){
+			throw new BusinessException(ExceptCodeConstants.Special.NO_RESULT, "订单不存在");
+		}
+		//3.2 修改订单主表状态 TODO
+		OrdOrder ordOrderUpdate = new OrdOrder();
+		ordOrderUpdate.setOrderId(request.getBaseInfo().getOrderId());
+		ordOrderUpdate.setState(request.getBaseInfo().getState());
+		this.ordOrderAtomSV.updateByPrimaryKeySelective(ordOrderUpdate);
+		//4.入库订单轨迹表 53：已评价（瞬时状态，记录轨迹）90：完成
+		OrdOdStateChg ordOdStateChg = new OrdOdStateChg();
+		ordOdStateChg.setStateChgId(SequenceUtil.createStateChgId());
+		ordOdStateChg.setOrderId(request.getBaseInfo().getOrderId());
+		ordOdStateChg.setChgDesc("订单 "+request.getBaseInfo().getOrderId()+" 已评价");
+		ordOdStateChg.setChgDescEn("");
+		ordOdStateChg.setChgDescD("");
+		ordOdStateChg.setChgDescUEn("");
+		ordOdStateChg.setFlag(OrdOdStateChgConstants.FLAG_USER);
+		ordOdStateChg.setOrgId("1");
+		ordOdStateChg.setOperId(request.getBaseInfo().getUserId());
+		ordOdStateChg.setOperName(request.getBaseInfo().getOperName());
+		ordOdStateChg.setOrgState(ordOrderDb.getState());
+		ordOdStateChg.setNewState(request.getBaseInfo().getState());
+		ordOdStateChg.setStateChgTime(DateUtil.getSysDate());
+		//
+		this.ordOdStateChgAtomSV.insertSelective(ordOdStateChg);
+		//
+		response.setOrderId(request.getBaseInfo().getOrderId());
+		//
+		return response;
 	}
 
 }
