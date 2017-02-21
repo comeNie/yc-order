a1f9acde436517fbd8a266315000d98a2f4a8b83
fazhicao <409993207@qq.com>
2017-02-21 18:00:23 +0800

    添加评价Id方法

--- order-core/src/main/java/com/ai/yc/order/util/SequenceUtil.java
+++ order-core/src/main/java/com/ai/yc/order/util/SequenceUtil.java
@@ -42,6 +42,8 @@ public final class SequenceUtil {
     
     private static final String ORD_OD_PERSON_INFO$PERSON_ID$SEQ = "ORD_OD_PERSON_INFO$PERSON_ID$SEQ";
     
+    private static final String ORD_EVALUATE$EVALUATE_ID$SEQ = "ORD_EVALUATE$EVALUATE_ID$SEQ";
+    
     public static Long createOrderId() {
         String seq = SeqUtil.getNewId(ORD_ORDER$ORDER_ID$SEQ, 10);
         String orderId = "2" + seq + SequenceUtil.returnRandom();
@@ -83,7 +85,9 @@ public final class SequenceUtil {
     public static Long createPersonId(){
     	return SeqUtil.getNewId(ORD_OD_PERSON_INFO$PERSON_ID$SEQ);
     }
-    
+    public static Long createEvaluateId(){
+    	return SeqUtil.getNewId(ORD_EVALUATE$EVALUATE_ID$SEQ);
+    }
 
     public static Long createFeeOffsetId() {
         return SeqUtil.getNewId(ORD_OD_FEE_OFFSET$FEE_OFFSET_ID$SEQ);
