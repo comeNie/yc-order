2d9e20ec9b71e401e6fe3d17044ff7120d39b24f
fazhicao <409993207@qq.com>
2017-02-21 17:59:44 +0800

    订单评价业务写入

--- order-core/src/main/java/com/ai/yc/order/service/business/interfaces/IOrdEvaluateBusiSV.java
+++ order-core/src/main/java/com/ai/yc/order/service/business/interfaces/IOrdEvaluateBusiSV.java
@@ -1,10 +1,14 @@
 package com.ai.yc.order.service.business.interfaces;
 
+import com.ai.yc.order.api.orderevaluation.param.OrderEvaluationRequest;
+import com.ai.yc.order.api.orderevaluation.param.OrderEvaluationResponse;
 import com.ai.yc.order.api.orderevaluation.param.QueryOrdEvaluteRequest;
 import com.ai.yc.order.api.orderevaluation.param.QueryOrdEvaluteResponse;
 
 public interface IOrdEvaluateBusiSV {
 	
 	public QueryOrdEvaluteResponse queryEvaluteByOrdId(QueryOrdEvaluteRequest request);
+
+	public OrderEvaluationResponse orderEvaluation(OrderEvaluationRequest request);
 
 }
